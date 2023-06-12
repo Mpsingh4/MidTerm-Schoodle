569260b5a1f292e47dd42622832f2e161da7be80
Manpreet <mp_manpreet@hotmail.com>
2023-06-12 23:51:10 +0000

    basic html and css skeleton

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,27 @@
 // Client facing scripts here
+
+// not working scripts yet just the idea, need a lot more work to actually function
+
+
+document.getElementById('event-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get form values
+  var eventTitle = document.getElementById('event-title').value;
+  var eventDescription = document.getElementById('event-description').value;
+  var organizerName = document.getElementById('organizer-name').value;
+  var organizerEmail = document.getElementById('organizer-email').value;
+
+  // Create an event object
+  var event = {
+    title: eventTitle,
+    description: eventDescription,
+    organizer: {
+      name: organizerName,
+      email: organizerEmail
+    }
+  };
+
+  // Do something with the event object (e.g., send it to the server)
+  console.log(event);
+});
